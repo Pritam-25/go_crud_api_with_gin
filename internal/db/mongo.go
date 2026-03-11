37a70e05de999bb2de322af4404f22fa9e7f83e1
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-11 19:22:13 +0530

    feat: add MongoDB connection setup

---
+++ internal/db/mongo.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Pritam-25/go_crud_api_with_gin/internal/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+)
+
+func ConnectMongoDB(cfg *config.Config) (*mongo.Client, *mongo.Database, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(cfg.MongoURI)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil, nil, fmt.Errorf("mongo connect failed: %w", err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, nil, fmt.Errorf("mongo ping failed: %w", err)
+	}
+
+	db := client.Database(cfg.MongoDBName)
+
+	return client, db, nil
+}
+
+func DisconnectMongoDB(client *mongo.Client) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.Disconnect(ctx)
+}
