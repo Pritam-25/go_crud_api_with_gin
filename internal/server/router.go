6dab47861e19d17b055588f06be45f203df971c1
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-11 19:22:36 +0530

    feat: add API routes and server setup

---
+++ internal/server/router.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/Pritam-25/go_crud_api_with_gin/internal/handler"
+	"github.com/Pritam-25/go_crud_api_with_gin/internal/repository"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func NewRouter(db *mongo.Database) *gin.Engine {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Welcome to Go CRUD API",
+		})
+	})
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	notRepo := repository.NewNoteRepository(db)
+	noteHandler := handler.NewNotesHandler(notRepo)
+
+	router.GET("/notes", noteHandler.GetNotes)
+	router.POST("/notes", noteHandler.CreateNote)
+
+	return router
+}
