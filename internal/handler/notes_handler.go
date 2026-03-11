0f8c27bbd6c5d0ab53ecf3d65d418afa7719a9e7
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-11 19:22:30 +0530

    feat: add notes handler

---
+++ internal/handler/notes_handler.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Pritam-25/go_crud_api_with_gin/internal/models"
+	"github.com/Pritam-25/go_crud_api_with_gin/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
+type NotesHandler struct {
+	repo *repository.NoteRepository
+}
+
+func NewNotesHandler(repo *repository.NoteRepository) *NotesHandler {
+	return &NotesHandler{
+		repo: repo,
+	}
+}
+
+func (h *NotesHandler) GetNotes(c *gin.Context) {
+	notes, err := h.repo.GetAllNotes(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error": "failed to fetch notes",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    notes,
+	})
+}
+
+func (h *NotesHandler) CreateNote(c *gin.Context) {
+	var req models.CreateNoteRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   "invalid request body",
+		})
+		return
+	}
+
+	note, err := h.repo.CreateNote(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "failed to create note",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"success": true,
+		"data":    note,
+	})
+}
