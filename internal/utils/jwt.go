c375d2fb0b583ac5d8a6e0cd021b00aad299e53a
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-14 01:17:36 +0530

    feat: add JWT token generation and password hashing functions

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,1 +1,30 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID string, jwtSecret string, role string) (string, error) {
+
+	var now = time.Now().UTC()
+
+	claims := Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   userID,
+			IssuedAt: jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(24 * time.Hour)),
+		},
+		Role: role,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(jwtSecret))
+}
--- internal/utils/password.go
+++ internal/utils/password.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, error := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), error
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
